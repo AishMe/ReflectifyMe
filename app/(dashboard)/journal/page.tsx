d445b81f804a6f093592cb42fb1d126ea34c2532
AishwaryaMenon15 <aishwarya.menon.15@gmail.com>
2023-08-16 11:42:26 +0530

    Initial Commit

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,12 @@
+export default function Journal() {
+    return (
+        <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+            <div className="w-full max-w-[780px] mx-auto">
+                <h1 className="text-5xl text-center mb-8 leading-tight">
+                    Nurturing Your Inner Self: <br/>A Journey of Reflection and Emotional Discovery
+                </h1>
+            </div>
+        </div>
+    )
+  }
+  
